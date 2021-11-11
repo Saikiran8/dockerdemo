8fc4831e4c066538e052514b125f8a4b4dc1c423
saikiran8 <43983464+Saikiran8@users.noreply.github.com>
2021-11-12 01:06:38 +0530

    added images

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     print("Hello World")
-    return "Hello World!"
+    #return "Hello World!"
+    return render_template('index.html')
 
 @app.route("/1")
 def hello1():
